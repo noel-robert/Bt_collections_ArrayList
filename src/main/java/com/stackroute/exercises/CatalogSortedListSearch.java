8c11c3b4d7021820c8ebd7ff750017513f6617e8
shahid <shahdbest@gmail.com>
2022-08-18 14:53:15 +0530

    a

---
+++ src/main/java/com/stackroute/exercises/CatalogSortedListSearch.java
@@ -0,0 +1,21 @@
+package com.stackroute.exercises;
+
+
+import java.util.List;
+
+public class CatalogSortedListSearch {
+
+
+    //write here logic to get sorted Array List which is a global class variable
+    public String catalogListSorter(List<String> unSortedCatalogList) {
+
+        return null;
+    }
+
+    //write here logic to search the input value in sorted Array List
+    public String catalogListSearcher(String value) {
+
+        return null;
+    }
+}
+
